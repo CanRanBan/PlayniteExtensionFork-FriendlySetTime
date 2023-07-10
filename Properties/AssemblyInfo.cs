a1940500436c1db087ea3b178a2de484009000c8
CanRanBan <canranban@gmx.de>
2023-07-10 04:10:10 +0200

    Reflect original author and current maintainer in assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FriendlySetTime")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Friendly Set Playtime")]
+[assembly: AssemblyDescription("Friendly Set Playtime for Playnite by erys | Updates by CanRanBan")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FriendlySetTime")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Friendly Set Playtime for Playnite")]
+[assembly: AssemblyCopyright("Copyright © 2021-2022 - erys | Updates 2023 - CanRanBan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
